a8d55e6266bc2db43f7464b00769856a80fe86c0
ajay <ajaykumar290904@gmail.com>
2024-11-07 18:18:21 +0530

    Brand page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+<div
+className="logo">
+  <img src="/images/brand.png" alt="brand" />
+</div>
+      </nav>
+    </div>
+  )
+}
+
+export default App;
