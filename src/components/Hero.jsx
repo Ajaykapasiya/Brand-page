9ec5f57981cdc5c82de7e69ad9f8babedb9c0901
ajay <ajaykumar290904@gmail.com>
2024-11-08 10:39:10 +0530

    Add the images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,31 @@
 const Hero = () => {
-    return <main className="hero-section">
-        <div className="hero-content">
-            <h1>YOUR FEET DESERVE THE BEST</h1>
-            <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES</p>
-        </div>
-        <div className="hero-btn">
-            <button>Shop now</button>
-            <button>Category</button>
-        </div>
+  return (
+    <main className="hero-section">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES
+        </p>
+      </div>
+      <div className="hero-btn">
+        <button>Shop now</button>
+        <button>Category</button>
+      </div>
 
-        <div className="shopping">
-            <p>Also Available on </p>
-        </div>
-        <div className="hero-image"></div>
+      <div className="shopping">
+        <p>Also Available on </p>
+      </div>
+      <div className="shop-icon">
+        <img src="/images/flipkart.png" alt="flipkart" />
+        <img src="/images/amazon.png" alt="amazon" />
+      </div>
+      <div className="hero-image">
+        <img src="/images/shoe_image.png" alt="" />
+      </div>
     </main>
-}
+  );
+};
 
 export default Hero;
